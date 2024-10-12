dd8c949f3fc0663ea0aea4d0180fb2834e178c6d
Akshar-2002 <akshar.tumu@gmail.com>
2024-10-12 21:23:41 +0000

    removed ws

--- cse276a_ws/build/key_joy/CMakeFiles/key_joy_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/key_joy_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/key_joy_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
