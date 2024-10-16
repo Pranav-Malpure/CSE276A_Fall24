614ae47dd021520c60978e635d56ac47f4f56d5c
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-10-16 15:13:20 -0700

    werwrf

--- cse276a_ws/src/rb5_ros2/rb5_ros2_control/rb5_ros2_control/rb5_control_node_big.py
+++ cse276a_ws/src/rb5_ros2/rb5_ros2_control/rb5_ros2_control/rb5_control_node_big.py
@@ -22,7 +22,7 @@ waypoints = [
 
 
 initial_time = 0
-current_pose = np.array([0,0,0])
+current_pose = [0,0,0]
 sleep_time = 1
 
 
@@ -133,7 +133,7 @@ class MegaPiControllerNode(Node):
 
                 # TODO: Call self.mpi_ctrl's setFourMotors(self, vfl=0, vfr=0, vbl=0, vbr=0) method, but clarify why some of the parameters are being passed as negative to the motor
                 #time.sleep(1)
-                #self.mpi_ctrl.carStraight(-100, 100, 100, -100)
+                self.mpi_ctrl.carStraight(int(-omega1), int(omega2)), int(omega3), int(-omega4)
                 #print("this is also working>>>")
 
                 # self.mpi_ctrl.setFourMotors(-100, 100, 100, -100)
