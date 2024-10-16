346f082267cdc1aad2ac1898cc063ac818908ab8
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-10-15 18:11:55 -0700

    changes

--- cse276a_ws/src/rb5_ros2/rb5_ros2_control/rb5_ros2_control/mpi_control.py
+++ cse276a_ws/src/rb5_ros2/rb5_ros2_control/rb5_ros2_control/mpi_control.py
@@ -100,10 +100,13 @@ if __name__ == "__main__":
     # mpi_ctrl.carStraight(100)
     # mpi_ctrl.setFourMotors(-vspeed, vspeed, vspeed, -vspeed)
     # time.sleep(2)
-    for i in range(12):
-        rspeed = 80
-        mpi_ctrl.setFourMotors(-rspeed, rspeed, -rspeed, rspeed) # for rotation
-        time.sleep(1/2)
+    mpi_ctrl.setFourMotors(-20, 20, 20, -20)
+    time.sleep(2)
+    mpi_ctrl.setFourMotors(-20, 20, -20, 20)
+    # for i in range(12):
+    #     rspeed = 80
+    #     mpi_ctrl.setFourMotors(-rspeed, rspeed, -rspeed, rspeed) # for rotation
+    #     time.sleep(1/2)
     # mpi_ctrl.carSlide(30)
     # time.sleep(1)
     # mpi_ctrl.carRotate(30)
