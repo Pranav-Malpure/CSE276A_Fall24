bf71afa901f3b9863d74fa8b328d67a57f275a14
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-10-16 16:11:54 -0700

    changed map max and min

--- cse276a_ws/src/rb5_ros2/rb5_ros2_control/rb5_ros2_control/rb5_control_node_big.py
+++ cse276a_ws/src/rb5_ros2/rb5_ros2_control/rb5_ros2_control/rb5_control_node_big.py
@@ -65,10 +65,10 @@ class MegaPiControllerNode(Node):
         # return values between 30 to 60 in proportion of the current omega values
 
         sum = abs(omega1) + abs(omega2) + abs(omega3) + abs(omega4)
-        ret_omega1 = int(omega1*60/sum)//2 +30
-        ret_omega2 = int(omega2*60/sum)//2 +30
-        ret_omega3 = int(omega3*60/sum)//2 +30
-        ret_omega4 = int(omega4*60/sum)//2 +30
+        ret_omega1 = int(omega1*80/sum)//2 + 40
+        ret_omega2 = int(omega2*80/sum)//2 + 40
+        ret_omega3 = int(omega3*80/sum)//2 + 40
+        ret_omega4 = int(omega4*80/sum)//2 + 40
 
 
         return [ret_omega1, ret_omega2, ret_omega3, ret_omega4]
