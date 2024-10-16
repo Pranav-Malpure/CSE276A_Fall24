6db94fc9aabf644e58b52a14975d084f5b493900
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-10-16 16:20:47 -0700

    wdqdcq

--- cse276a_ws/src/rb5_ros2/rb5_ros2_control/rb5_ros2_control/rb5_control_node_big.py
+++ cse276a_ws/src/rb5_ros2/rb5_ros2_control/rb5_ros2_control/rb5_control_node_big.py
@@ -10,20 +10,30 @@ import time
 
 
 
+# waypoints = [
+# [0,0,0],
+# [-1,0,0],
+# [-1,1,1.57],
+# [-2,1,0],
+# [-2,2,-1.57],
+# [-1,1,-0.78],
+# [0,0,0]
+# ]
+
 waypoints = [
 [0,0,0],
-[-1,0,0],
-[-1,1,1.57],
-[-2,1,0],
-[-2,2,-1.57],
-[-1,1,-0.78],
+[-1/2,0,0],
+[-1/2,1/2,1.57],
+[-2/2,1/2,0],
+[-2/2,2/2,-1.57],
+[-1/2,1/2,-0.78],
 [0,0,0]
 ]
 
 
 initial_time = 0
 current_pose = [0,0,0]
-sleep_time = 2
+sleep_time = 0.5
 
 
 Kv = 0.1 # this is the factor which gets multiplied with linear velocity to give the number to pass to the carStraight function, has to be callibrated
@@ -80,9 +90,8 @@ class MegaPiControllerNode(Node):
         print("hello1")
         try:
             while True:
-                print("hello2")
-                print (linear_distance)
-                print (threshold_distance)
+                print ("linear_dst",linear_distance)
+                print ("threshold_distance",threshold_distance)
                 if linear_distance < threshold_distance:
                     # if self.calc_diff_theta(waypoints[waypoints_index][2], current_pose[2]) > 0.05:
                     #     # calculate omega that will make the robot rotate towards the waypoint
@@ -110,15 +119,12 @@ class MegaPiControllerNode(Node):
                 # time.sleep(0.5) #callibrate this
                 # self.mpi_ctrl.carStop()
 
-
-
-                print("hello4")
                 current_waypoint = waypoints[waypoints_index]
                 print("current_waypoint = ", current_waypoint)
                 print("current_pose = ", current_pose)            
                 linear_distance = np.sqrt((current_waypoint[0] - current_pose[0])**2 + (current_waypoint[1] - current_pose[1])**2)
                 print("linear_distance = ", linear_distance)
-                print("hello4v")
+
                 theta_target = np.arctan2(current_waypoint[1] - current_pose[1], current_waypoint[0] - current_pose[0])
 
 
@@ -147,9 +153,7 @@ class MegaPiControllerNode(Node):
                 #print("this is also working>>>")
 
                 # self.mpi_ctrl.setFourMotors(-100, 100, 100, -100)
-                print("hello5")
                 time.sleep(sleep_time)
-                print("hello6")
                 #current_pose[2] = (current_pose[2] + theta_target/sleep_time * sleep_time) #Find out theta range from instructors
                 current_pose[2] = self.get_under_range(current_pose[2] + gamma)
                 current_pose[0] = current_pose[0] + v_target * np.cos(current_pose[2]) * sleep_time
