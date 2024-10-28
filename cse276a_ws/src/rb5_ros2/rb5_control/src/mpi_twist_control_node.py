91cfcc85bcd747698449af3c0a968e5f115b8830
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-10-28 13:21:29 -0700

    updates for hw1 solution

--- cse276a_ws/src/rb5_ros2/rb5_control/src/mpi_twist_control_node.py
+++ cse276a_ws/src/rb5_ros2/rb5_control/src/mpi_twist_control_node.py
@@ -17,7 +17,7 @@ class MegaPiControllerNode(Node):
         self.lx = 0.055 # half of the distance between front wheel and back wheel
         self.ly = 0.07 # half of the distance between left wheel and right wheel
         self.calibration_x = 125
-        self.calibration_y = 210
+        self.calibration_y = 213
         self.calibration_ang = 0
         self.subscription = self.create_subscription(Twist, '/twist', self.twist_callback, 10)
         self.subscription
