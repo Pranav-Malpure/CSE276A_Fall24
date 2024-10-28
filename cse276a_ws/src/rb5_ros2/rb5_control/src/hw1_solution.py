f615db127d37c5ff29d432777a17c605afa9cd60
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-10-28 12:03:45 -0700

    updates for hw1 solution

--- cse276a_ws/src/rb5_ros2/rb5_control/src/hw1_solution.py
+++ cse276a_ws/src/rb5_ros2/rb5_control/src/hw1_solution.py
@@ -116,7 +116,7 @@ if __name__ == "__main__":
                          [0.0,0.0,0.0]]) 
 
     # init pid controller
-    pid = PIDcontroller(0.05,0.005,0.05)
+    pid = PIDcontroller(0.01,0.005,0.9)
     print("kp", pid.Kp, "ki", pid.Ki, "kd", pid.Kd)
     time.sleep(3)
     # init current state
