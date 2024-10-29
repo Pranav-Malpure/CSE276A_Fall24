f67a2e1ef0fd150afa37d6b3c1dba28bdfac898c
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-10-29 11:59:49 -0700

    hw1_solution updates and fixes

--- cse276a_ws/src/rb5_ros2/rb5_control/src/hw1_solution.py
+++ cse276a_ws/src/rb5_ros2/rb5_control/src/hw1_solution.py
@@ -119,7 +119,7 @@ if __name__ == "__main__":
     # waypoint = np.array([[0.0,0.0,0.0], [0.0, 0.0, np.pi/2.0]])
 
     # init pid controller
-    pid = PIDcontroller(0.02, 0.0015,0.035)
+    pid = PIDcontroller(0.02, 0.015,0.035)
     print("kp", pid.Kp, "ki", pid.Ki, "kd", pid.Kd)
     time.sleep(3)
     # init current state
