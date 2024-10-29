703411bab23e49e2071d318baea57f077f8cf360
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-10-29 12:59:54 -0700

    hw1_solution updates and fixes

--- cse276a_ws/src/rb5_ros2/rb5_control/src/hw1_solution.py
+++ cse276a_ws/src/rb5_ros2/rb5_control/src/hw1_solution.py
@@ -62,10 +62,10 @@ class PIDcontroller(Node):
         """
         e = self.getError(currentState, self.target)
 
-        P = np.array([self.Kp * e[0]/0.5, self.Kp * e[1]/0.5, self.Kp * e[2]/1.5])
+        P = np.array([self.Kp * e[0]/0.5, self.Kp * e[1]/0.5, self.Kp * e[2]/2])
         self.I = self.I + self.Ki * e * self.timestep 
         I = self.I
-        D = self.Kd * (e - self.lastError)
+        D = np.array([self.Kd * (e[0] - self.lastError[0]), self.Kd * (e[1] - self.lastError[1])/1.5, self.Kd * (e[2] - self.lastError[2])/1.5])
         result = P + I + D
 
         self.lastError = e
