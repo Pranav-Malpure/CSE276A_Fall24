1ff5bf8c8e82baa2d18da5585379eb8d3660f5f9
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-10-29 18:42:25 -0700

    debugging final scripts for hw2

--- cse276a_ws/src/rb5_ros2/rb5_control/src/hw1_solution.py
+++ cse276a_ws/src/rb5_ros2/rb5_control/src/hw1_solution.py
@@ -234,7 +234,7 @@ if __name__ == "__main__":
             time.sleep(0.05)
             # update the current state
             # current_state += update_value
-            # pid.publisher_.publish(genTwistMsg(np.array([0.0,0.0,0.0])))
+            pid.publisher_.publish(genTwistMsg(np.array([0.0,0.0,0.0])))
             pid.wait_for_new_pose(update_value)
             print("current_state = ", pid.current_state)
             # print("update value",update_value)
