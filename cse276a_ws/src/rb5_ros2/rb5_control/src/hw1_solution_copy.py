7a08cb5575d21354d98023a56eb9e39374666e51
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-10-29 19:29:23 -0700

    debugging final scripts for hw2

--- cse276a_ws/src/rb5_ros2/rb5_control/src/hw1_solution_copy.py
+++ cse276a_ws/src/rb5_ros2/rb5_control/src/hw1_solution_copy.py
@@ -94,8 +94,8 @@ def genTwistMsg(desired_twist):
     return twist_msg
 
 def coord(twist, current_state):
-    J = np.array([[np.cos(current_state[2]), np.sin(current_state[2]), 0.0],
-                  [-np.sin(current_state[2]), np.cos(current_state[2]), 0.0],
+    J = np.array([[np.cos(current_state[2]-np.pi/2), np.sin(current_state[2]-np.pi/2), 0.0],
+                  [-np.sin(current_state[2]-np.pi/2), np.cos(current_state[2]-np.pi/2), 0.0],
                   [0.0,0.0,1.0]])
     return np.dot(J, twist)
     
