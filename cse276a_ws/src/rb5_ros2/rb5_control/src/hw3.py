e190f110c849a598ff5281becf4eda5506bae146
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-11-10 16:42:05 -0800

    hw3 starts here

--- cse276a_ws/src/rb5_ros2/rb5_control/src/hw3.py
+++ cse276a_ws/src/rb5_ros2/rb5_control/src/hw3.py
@@ -145,7 +145,7 @@ def main():
             twist_msg = Twist()
             twist_msg.linear.x = 0.0
             twist_msg.linear.y = 0.0
-            twist_msg.linear.z = 0.1
+            twist_msg.linear.z = 1.0
             twist_msg.angular.x = 0.0
             twist_msg.angular.y = 0.0
             twist_msg.angular.z = 0.0
