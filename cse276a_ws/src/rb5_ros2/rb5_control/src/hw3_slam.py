204dbec76e7364da099c661367286dd9bd332ae7
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-11-10 18:55:56 -0800

    bugs

--- cse276a_ws/src/rb5_ros2/rb5_control/src/hw3_slam.py
+++ cse276a_ws/src/rb5_ros2/rb5_control/src/hw3_slam.py
@@ -127,11 +127,11 @@ class KalmanFilter():
     def update(self):
         self.K_t = np.dot( np.dot(self.variance_update, self.H.T), np.linalg.pinv(np.dot( np.dot(self.H, self.variance_update), self.H.T)  + self.R) )
         # print("K_t", self.K_t)
-        print("CAPITAL S", np.dot( np.dot(self.H, self.variance_update), self.H.T)  + self.R)
+        # print("CAPITAL S", np.dot( np.dot(self.H, self.variance_update), self.H.T)  + self.R)
         self.state = self.state_update + np.dot(self.K_t, (self.z - np.dot(self.H, self.state_update)))
 
         self.variance = np.dot(np.identity(53) - np.dot(self.K_t, self.H), self.variance)
-
+        print("variance", self.variance)
         # return self.next_state
 
 
