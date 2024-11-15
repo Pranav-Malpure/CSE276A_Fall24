2e16122e628ca2cf72f9dc8486377c24e6254e64
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-11-14 23:11:24 -0800

    debugging

--- cse276a_ws/src/rb5_ros2/rb5_control/src/hw3.py
+++ cse276a_ws/src/rb5_ros2/rb5_control/src/hw3.py
@@ -63,7 +63,7 @@ class PIDcontroller(Node):
 
     def get_measurement(self, kf):
         rclpy.spin_once(self)
-        if int(self.callback_data[2]) > 15:
+        while int(self.callback_data[2]) > 15:
             rclpy.spin_once(self)
         time.sleep(0.1)
         # print("callback data", self.callback_data)
@@ -335,7 +335,7 @@ def main():
         seen_tags = set()
         for _ in range(25):
             rclpy.spin_once(pid)
-            if int(pid.callback_data[2]) > 15:
+            while int(pid.callback_data[2]) > 15:
                 rclpy.spin_once(pid)
             time.sleep(0.1)
             frame_id, pitch = pid.callback_data[2], pid.callback_data[3]
@@ -400,7 +400,7 @@ def main():
                 it_seen = set()
                 for _ in range(25):   
                     rclpy.spin_once(pid)  
-                    if int(pid.callback_data[2]) > 15:
+                    while int(pid.callback_data[2]) > 15:
                         rclpy.spin_once(pid)
                     time.sleep(0.1)      
                     frame_id, pitch = pid.callback_data[2], pid.callback_data[3]
@@ -487,7 +487,7 @@ def main():
                         it_seen = set()
                         for _ in range(25):      
                             rclpy.spin_once(pid)   
-                            if int(pid.callback_data[2]) > 15:
+                            while int(pid.callback_data[2]) > 15:
                                 rclpy.spin_once(pid)    
                             frame_id, pitch = pid.callback_data[2], pid.callback_data[3]
                             kf.newpit[int(frame_id) - 1] = pitch
