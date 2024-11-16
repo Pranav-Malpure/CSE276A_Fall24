a09787cc65f1e00d9ae48d17aaba7ea1ab959fc9
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-11-16 14:33:15 -0800

    added dummy subscriber

--- cse276a_ws/src/rb5_ros2/rb5_control/src/dummy_subscribe.py
+++ cse276a_ws/src/rb5_ros2/rb5_control/src/dummy_subscribe.py
@@ -13,35 +13,44 @@ import math
 from collections import defaultdict
 import pickle
 
-callback_data = defaultdict(dict)
 
-def pose_callback(msg):
-    x = msg.pose.position.x
-    z = msg.pose.position.z
-    x_ang = msg.pose.orientation.x
-    y_ang = msg.pose.orientation.y
-    z_ang = msg.pose.orientation.z
-    w_ang = msg.pose.orientation.w
 
-    pitch = math.atan2(2 * (w_ang*y_ang - x_ang*z_ang), 1 - 2 * (y_ang*y_ang + z_ang*z_ang))
-    pitch = (pitch + math.pi) % (2 * math.pi) - math.pi # scale to range [-pi, pi)
 
-    frame_id = msg.header.frame_id
+class sdkf(Node):
     
-    callback_data[frame_id] = {'x': x, 'z': z, 'pitch': pitch}
 
-def subscriber():
-    rclpy.init()
-    node = rclpy.create_node('dummy_subscriber')
-    subscription = rclpy.create_subscription(
+
+    def __init__(self):
+        super().__init__('dummy_subscriber')
+        self.subscription = self.create_subscription(
             PoseStamped,
             '/april_poses',
-            pose_callback(),
-            10)
+            self.pose_callback,
+            10) 
+        self.callback_data = defaultdict(dict)
+        
+    def pose_callback(self, msg):
+        x = msg.pose.position.x
+        z = msg.pose.position.z
+        x_ang = msg.pose.orientation.x
+        y_ang = msg.pose.orientation.y
+        z_ang = msg.pose.orientation.z
+        w_ang = msg.pose.orientation.w
 
+        pitch = math.atan2(2 * (w_ang*y_ang - x_ang*z_ang), 1 - 2 * (y_ang*y_ang + z_ang*z_ang))
+        pitch = (pitch + math.pi) % (2 * math.pi) - math.pi # scale to range [-pi, pi)
+
+        frame_id = msg.header.frame_id
+        
+        self.callback_data[frame_id] = {'x': x, 'z': z, 'pitch': pitch}
+
+   
 
 if __name__ == '__main__':
-    subscriber()
+    rclpy.init()
+    a = sdkf()
+    rclpy.spin_once(a)
+
     for i in range(10):
         rclpy.spin_once()
-    print(callback_data)
+    print(a.callback_data)
