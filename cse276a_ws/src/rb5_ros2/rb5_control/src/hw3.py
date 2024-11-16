29ab38060e027313d138e387759dc0b89abe8425
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-11-15 21:21:51 -0800

    final testing begins

--- cse276a_ws/src/rb5_ros2/rb5_control/src/hw3.py
+++ cse276a_ws/src/rb5_ros2/rb5_control/src/hw3.py
@@ -460,7 +460,7 @@ def main():
                 twist_msg.linear.x = 0.0
                 
                 pid.publisher_.publish(twist_msg)
-                time.sleep(0.2)
+                time.sleep(0.5)
                 # Predict state in open loop
                 kf.predict(input)
 
@@ -549,7 +549,7 @@ def main():
                         # Stop Car
                         twist_msg.angular.z = 0.0
                         pid.publisher_.publish(twist_msg)
-                        time.sleep(0.2)
+                        time.sleep(0.5)
                         # Predict state in open loop
                         # kf.predict(input)
 
