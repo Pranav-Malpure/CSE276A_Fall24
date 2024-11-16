1fa8a881e7440d701ac23740bf6315d364843c8e
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-11-15 18:28:37 -0800

    final testing begins

--- cse276a_ws/src/rb5_ros2/rb5_control/src/hw3.py
+++ cse276a_ws/src/rb5_ros2/rb5_control/src/hw3.py
@@ -375,7 +375,7 @@ def main():
                 print("wp robot frame", wp_robot_frame)
 
                 # if (np.linalg.norm(pid.getError(kf.state[0:3], wp)[:2]) > 0.15):
-                if abs(kf.state[1][0] - wp[1]) > 0.15:
+                if abs(robot_frame_state[1] - wp_robot_frame[1]) > 0.15:
                     twist_msg.linear.x = 0.0
                     twist_msg.linear.y = 0.04*pid.update_sign(robot_frame_state, wp_robot_frame)[1]
                     twist_msg.linear.z = 0.0
@@ -389,7 +389,7 @@ def main():
                     input_x = -np.sin(theta_)*calibration_y*twist_msg.linear.y/1.1
                     input_y = np.cos(theta_)*calibration_y*twist_msg.linear.y/1.1
                     input_y_moved = np.array(([input_x], [input_y], [calibration_ang*twist_msg.angular.z/1.45]))
-                elif abs(kf.state[1][0] - wp[1]) <= 0.15:
+                elif abs(robot_frame_state[1] - wp_robot_frame[1]) <= 0.15:
                     theta_ = kf.state[2][0]
                     twist_msg.linear.x = 0.0
                     twist_msg.linear.y = 0.02*pid.update_sign(robot_frame_state, wp_robot_frame)[1]
@@ -410,8 +410,8 @@ def main():
                 time.sleep(0.2)
 
                 input_x_moved = np.array(([0], [0], [0]))
-                if abs(kf.state[1][0] - wp[1]) < 0.05:
-                    if abs(kf.state[0][0] - wp[0]) > 0.05:
+                if abs(robot_frame_state[1] - wp_robot_frame[1]) < 0.05:
+                    if abs(robot_frame_state[0] - wp_robot_frame[0]) > 0.05:
                         print('inside x correction')
                         theta_ = kf.state[2][0]
                         twist_msg.linear.x = 0.05*pid.update_sign(robot_frame_state, wp_robot_frame)[0]
