309d987dfd75142e2f49f3cbb37a695b398b3fe7
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-11-16 16:57:41 -0800

    debugging

--- cse276a_ws/src/rb5_ros2/rb5_control/src/hw3_slam.py
+++ cse276a_ws/src/rb5_ros2/rb5_control/src/hw3_slam.py
@@ -156,8 +156,8 @@ def main():
     time.sleep(3)
     for _ in range(1):
         twist_msg = Twist()
-        twist_msg.linear.x = 0.05
-        twist_msg.linear.y = 0.0
+        twist_msg.linear.x = 0.0
+        twist_msg.linear.y = 0.01
         twist_msg.linear.z = 0.0
         twist_msg.angular.x = 0.0
         twist_msg.angular.y = 0.0
@@ -170,7 +170,7 @@ def main():
 
         input = np.array(([calibration_x*twist_msg.linear.x/9], [calibration_y*twist_msg.linear.y/1.3], [calibration_ang*twist_msg.angular.z/1.45]))
         # Stop Car
-        twist_msg.linear.x = 0.0
+        twist_msg.linear.y = 0.0
         pid.publisher_.publish(twist_msg)
         time.sleep(0.2)
         # Predict state in open loop
