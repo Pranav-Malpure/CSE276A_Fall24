ec1f263c909adab62df428620cd7ec1fe3a3a5b3
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-11-16 16:38:39 -0800

    debugging

--- cse276a_ws/src/rb5_ros2/rb5_control/src/hw3.py
+++ cse276a_ws/src/rb5_ros2/rb5_control/src/hw3.py
@@ -545,7 +545,7 @@ def main():
                     # err1 = abs(kf.state[2][0] - wp[2])
                     # err2 = abs((kf.state[2][0] + 2 * np.pi) % (2 * np.pi) - (wp[2] + 2 * np.pi) % (2 * np.pi))
 
-                    while rclpy.ok() and (abs(kf.state[2][0] - wp[2])) > 0.06:
+                    while rclpy.ok() and (abs(kf.state[2][0] - wp[2])) > 0.04:
                     # while rclpy.ok() and min(err1, err2) > 0.06:
                         robot_frame_state = [kf.state[0][0]*np.cos(kf.state[2][0]) + kf.state[1][0]*np.sin(kf.state[2][0]),
                                              -kf.state[0][0]*np.sin(kf.state[2][0]) + kf.state[1][0]*np.cos(kf.state[2][0]), kf.state[2][0]]
